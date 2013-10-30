70c3471291900beb07fae9ca206cb377bc92e77f
Killian Jimenez <alu0100502053@ull.edu.es>
2013-10-30 20:56:42 +0000

    Modificado racional.rb con el comparable

--- lib/math/racional.rb
+++ lib/math/racional.rb
@@ -1,10 +1,12 @@
 require_relative "gcd.rb"
 class Racional
 
+	include Comparable
    attr_accessor :numerador, :denominador
-   
+
    def initialize(numerador, denominador)
       @numerador, @denominador = numerador, denominador
+      reducir
    end
    def reducir
       mcd = gcd(@numerador, @denominador)
@@ -23,10 +25,6 @@ class Racional
    def flotante
       @numerador/@denominador
    end
-   def ==(other)
-      return @numerador == other.numerador && @denominador == other.denominador if other.instance_of? Racional
-      false
-   end
    def abs
       if @numerador < 0 then @numerador = @numerador * -1 end
       if @denominador < 0 then @denominador = @denominador * -1 end
@@ -43,53 +41,25 @@ class Racional
       mcm = (@denominador * other.denominador)/gcd(@denominador, other.denominador)
       @numerador = ((mcm/@denominador*@numerador) + (mcm/other.denominador*other.numerador))
       @denominador = mcm
-      reducir
    end
    def -(other)
       mcm = (@denominador * other.denominador)/gcd(@denominador, other.denominador)
       @numerador = ((mcm/@denominador*@numerador) - (mcm/other.denominador*other.numerador))
       @denominador = mcm
-      reducir
    end
    def *(other)
       @numerador = @numerador * other.numerador
       @denominador = @denominador * other.denominador
-      reducir
    end
    def /(other)
       @numerador = @numerador * other.denominador
       @denominador = @denominador * other.numerador
-      reducir
    end
    def %(other)
       (@numerador/@denominador)%(other.numerador/other.denominador)
    end
-   def menor(other)
-      mcm = (@denominador * other.denominador)/gcd(@denominador, other.denominador)
-      a = (mcm/@denominador*@numerador)
-      b = (mcm/other.denominador*other.numerador)
-      return @numerador < other.numerador
-      false
-   end
-   def mayor(other)
-      mcm = (@denominador * other.denominador)/gcd(@denominador, other.denominador)
-      a = (mcm/@denominador*@numerador)
-      b = (mcm/other.denominador*other.numerador)
-      return @numerador > other.numerador
-      false
-   end
-   def menorigual(other)
-      mcm = (@denominador * other.denominador)/gcd(@denominador, other.denominador)
-      a = (mcm/@denominador*@numerador)
-      b = (mcm/other.denominador*other.numerador)
-      return @numerador <= other.numerador
-      false
-   end
-   def mayorigual(other)
-      mcm = (@denominador * other.denominador)/gcd(@denominador, other.denominador)
-      a = (mcm/@denominador*@numerador)
-      b = (mcm/other.denominador*other.numerador)
-      return @numerador >= other.numerador
-      false
+   def <=>(other)
+   	@numerador <=> other.numerador
+   	@denominador <=> other.denominador
    end
 end
